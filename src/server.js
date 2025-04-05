9117824b620b3ecb1590458e9de4cf94c34ddba8
Fabián Borbón <47618344+AntiCheats57@users.noreply.github.com>
2025-04-04 23:51:16 -0600

    feat: add positions endpoint to get tournaments positions ordered

--- src/server.js
+++ src/server.js
@@ -1,35 +1,36 @@
 const express = require("express");
-const fs = require("fs");
 const cors = require("cors");
+const { getAll, getPositions } = require("./service/TournamentService.mjs");
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const JSON_FILE = "./src/assets/data.json";
 
-app.use(express.json()); // Permite recibir JSON en las peticiones
-app.use(cors()); // Permite acceso desde cualquier origen
+app.use(express.json());
+app.use(cors());
 
-// Leer el JSON
-app.get("/data", (req, res) => {
-  fs.readFile(JSON_FILE, "utf8", (err, data) => {
-    if (err) {
-      console.info(err.message)
-      return res.status(500).json({ error: "Error al leer el archivo" });
-    }
-    res.json(JSON.parse(data));
-  });
+app.get("/api/tournaments/all", (req, res) => {
+  let data = {}
+  getAll()
+    .then(response => {
+      res.json(JSON.parse(response))
+    })
+    .catch(error => {
+      console.info('Error | api/tournaments/all | Getting all data: ' + error.message)
+      res.status(500).send("Error getting all data")
+    })
 });
 
-// Modificar el JSON
-app.post("/update", (req, res) => {
-  const newData = req.body;
-
-  fs.writeFile(JSON_FILE, JSON.stringify(newData, null, 2), "utf8", (err) => {
-    if (err) {
-      return res.status(500).json({ error: "Error al escribir el archivo" });
-    }
-    res.json({ message: "JSON actualizado correctamente" });
-  });
+app.get("/api/tournaments/positions", async (req, res) => {
+  const data = await getPositions()
+  if (typeof data === 'undefined' || data === null) {
+    console.info('Error | /api/tornaments/positions | Getting positions')
+    res.status(500).send("Error getting positions")
+  }
+  else {
+    res.json(data)
+  }
 });
 
 app.listen(PORT, () => {
